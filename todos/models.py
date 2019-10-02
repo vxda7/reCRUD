e7e29cd876a4e14e9c2a2b0aa75ee7b122827034
vxda77 <vxda77@gmail.com>
2019-10-02 14:48:35 +0900

    모델 작성

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length = 50)
+    due_date = models.DateField(max_length = 50)
--- todos/views.py
+++ todos/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def new(request):
+    return render(request, 'new.html')
+
+# def create(request):
+
